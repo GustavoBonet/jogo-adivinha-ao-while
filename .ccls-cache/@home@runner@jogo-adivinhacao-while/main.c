ab0ef58f905fc0007ecc864d7a928cfda4f628fd
Gustavo Bonet <55928208+GustavoBonet@users.noreply.github.com>
2022-07-09 01:16:36 +0000

    melhorando e adicionando escolha de dificuldade

--- .ccls-cache/@home@runner@jogo-adivinhacao-while/main.c
+++ .ccls-cache/@home@runner@jogo-adivinhacao-while/main.c
@@ -17,14 +17,36 @@ int main(void) {
   //variavel para guardar a resposta do jogador
   int chute;
   // variavel para entrar no while
-  int ganhou = 0;
+  //int ganhou = 0;
   // variavel para contar quantas vezes o jogador tentou
-  int tentativas = 1;
+  int contadortentativas = 1;
   double pontos = 1000.0;
-  
+  int dificuldade;
+  int numerotentativas;
+
   // while(1) significa que o loop roda infinitamente ate ser parado pelo break
-  while(1){
-    printf("Digite um número:");
+  while (1){
+    printf("Escolha a dificuldade: \n(1) fácil \n(2) médio \n(3) difícil\n");
+    scanf("%d",&dificuldade);
+    
+    if(dificuldade == 1){
+      numerotentativas = 20;
+      break;
+    }else if (dificuldade == 2){
+      numerotentativas = 15;
+      break;
+    }else if (dificuldade == 3){
+      numerotentativas = 6;
+      break;
+    }else {
+      printf("Escolha inválida");
+      continue;
+    }
+  }
+  
+  // esse loop roda uma determinada quantidade de vezes
+  for(int i = 1; i <= numerotentativas; i++){
+    printf("\nDigite um número:");
     scanf("%d",&chute);
     
     // verifica se o chute do jogador foi menor ou igual a zero e volta pro começo do while se entrar nessa       funçao
@@ -33,7 +55,7 @@ int main(void) {
       continue;
     }
     //mostra quantas vezes o loop ja rodou
-    printf("%d tentativas\n\n", tentativas);
+    printf("%d tentativas\n\n", contadortentativas);
     
     if (numero_secreto == chute){
       printf("Parabéns você acertou!\n\n");
@@ -45,7 +67,7 @@ int main(void) {
       printf("O número secreto é maior\n");
     }
     // toda vez que ele passa aqui a variavel tentativas aumenta mais 1
-    tentativas++;
+    contadortentativas++;
     // a funçao abs transforma todo negativo em positivo
     double pontosperdidos = abs(chute - numero_secreto) / 2.0;
     pontos = pontos - pontosperdidos;
